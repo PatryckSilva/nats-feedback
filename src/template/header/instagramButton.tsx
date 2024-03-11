01fd0dafc61e99cd41f4d1ca8eac63980c2a7300
Patryck Silva <patrycksilva70@gmail.com>
2024-03-10 23:11:33 -0300

    fix: changing instagram link

--- src/template/header/instagramButton.tsx
+++ src/template/header/instagramButton.tsx
@@ -3,7 +3,7 @@ import { FaInstagram } from "react-icons/fa6";
 export const InstagramButton = () => {
   return (
     <span
-      onClick={() => open("https://www.instagram.com/_natdoces_/")}
+      onClick={() => open("https://www.instagram.com/a_natdoces_/")}
       className={`button !p-2`}
     >
       <FaInstagram size={25} />
